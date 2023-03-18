999f756f97d52d27c1cee54dc527399a28484f35
Charles Zhang <szinubuntu@gmail.com>
2023-03-18 18:07:41 -0400

    [Build] Setup Android publish script

--- Powershell/PublishAll.ps1
+++ Powershell/PublishAll.ps1
@@ -1,10 +1,13 @@
 $originalFolder = Get-Location
 Set-Location "$PSScriptRoot/../"
 
-Remove-Item Publish -Recurse
-New-Item -Path Publish -ItemType Directory
+$publishFolder = "Publish"
+if (Test-Path -Path $publishFolder) {
+	Remove-Item $publishFolder -Recurse
+}
+New-Item -Path $publishFolder -ItemType Directory
 
-$password = Read-Host "Enter Android Signing Key Pass" -AsSecureString
-dotnet publish Freesome/Freesome.csproj --framework net7.0-android --configuration Release --output Publish/Freesome /p:AndroidSigningKeyPass=$password /p:AndroidSigningStorePass=$password
+$password = Read-Host "Enter Android Signing Key Pass"
+dotnet publish Freesome/Freesome.csproj --framework net7.0-android --configuration Release --output "$publishFolder/Freesome" /p:AndroidSigningKeyPass=$password /p:AndroidSigningStorePass=$password /p:AndroidSigningKeyStore=freesome.keystore
 
 Set-Location $originalFolder
